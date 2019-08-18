310882d8beda536704fc8605dba913bc3a94529d
daphne178 <realta23dc@gmail.com>
2019-08-18 19:04:10 -0400

    added signup

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Navbar from './layout/Navbar'
 import Dashboard from './dashboard/Dashboard'
 import ProjectDetails from './project/ProjectDetails'
 import SignIn from './auth/SignIn'
+import SignUp from './auth/SignUp'
 
 class App extends Component {
   render () {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path='/' component={Dashboard} />
             <Route path='/project/:id' component={ProjectDetails} />
             <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
