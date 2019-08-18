02d0c76f7f7be35d8fe31e6c73490a4edd0ae474
daphne178 <realta23dc@gmail.com>
2019-08-18 18:16:51 -0400

    added project summary

--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -10,18 +10,6 @@ const ProjectSummary = () => {
         <p>Date</p>
       </div>
 
-      <div>
-        <span>Project Title</span>
-        <p>Posted by auther</p>
-        <p>Date</p>
-      </div>
-
-      <div>
-        <span>Project Title</span>
-        <p>Posted by auther</p>
-        <p>Date</p>
-      </div>
-
     </div>
   )
 }
