0b16ae5fff038d5a4cc59942af07e0aa9bc2da40
daphne178 <realta23dc@gmail.com>
2019-08-19 10:35:39 -0400

    added firestore in index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers.js/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import firebaseConfig from './config/firebaseConfig'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer,
+  compose( applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+  reduxFirestore(firebaseConfig),
+  reactReduxFirebase(firebaseConfig)
+  )
+)
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 serviceWorker.register();
