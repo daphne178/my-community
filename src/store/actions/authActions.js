610e97c2af62a4ebe5fdcc2d0811fc2841ed8f0d
daphne178 <realta23dc@gmail.com>
2019-08-19 20:32:31 -0400

    fixed bug

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,46 +1,47 @@
 export const signIn = (credentials) => {
   return (dispatch, getState, {getFirebase}) => {
-    const firebase = getFirebase()
+    const firebase = getFirebase();
 
     firebase.auth().signInWithEmailAndPassword(
       credentials.email,
       credentials.password
-    ).then(() =>{
-      dispatch({ type: 'LOGIN_SUCCESS' })
+    ).then(() => {
+      dispatch({ type: 'LOGIN_SUCCESS' });
     }).catch((err) => {
-      dispatch({ type: 'LOGIN_ERROR', err})
-    })
+      dispatch({ type: 'LOGIN_ERROR', err });
+    });
+
   }
 }
 
 export const signOut = () => {
   return (dispatch, getState, {getFirebase}) => {
-    const firebase = getFirebase()
+    const firebase = getFirebase();
 
     firebase.auth().signOut().then(() => {
       dispatch({ type: 'SIGNOUT_SUCCESS' })
-    })
+    });
   }
 }
 
 export const signUp = (newUser) => {
   return (dispatch, getState, {getFirebase, getFirestore}) => {
-    const firebase = getFirebase()
-    const firestore = getFirestore()
+    const firebase = getFirebase();
+    const firestore = getFirestore();
 
-    firebase.auth().createUserWithEmailAndPassword().then(
+    firebase.auth().createUserWithEmailAndPassword(
       newUser.email,
       newUser.password
-    ).then((res) =>{
+    ).then(res => {
       return firestore.collection('users').doc(res.user.uid).set({
         firstName: newUser.firstName,
         lastName: newUser.lastName,
         initials: newUser.firstName[0] + newUser.lastName[0]
-      })
+      });
     }).then(() => {
-      dispatch({ type: 'SIGNUP_SUCCESS' })
+      dispatch({ type: 'SIGNUP_SUCCESS' });
     }).catch((err) => {
-      dispatch({ type: 'SIGNUP_ERROR', err})
-    })
+      dispatch({ type: 'SIGNUP_ERROR', err});
+    });
   }
 }
