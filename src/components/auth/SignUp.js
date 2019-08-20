52000a790232f708daa636726d70d898620dcb64
daphne178 <realta23dc@gmail.com>
2019-08-19 20:02:13 -0400

    connected signup to firebase

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import { signUp } from '../../store/actions/authActions'
 
 class SignUp extends Component {
   state = {
@@ -18,12 +19,12 @@ class SignUp extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
-    console.log(this.state)
+    this.props.signUp(this.state)
   }
 
   render() {
 
-    const { auth } = this.props
+    const { auth, authError } = this.props
 
     if(auth.uid){
       return <Redirect to='/' />
@@ -52,6 +53,9 @@ class SignUp extends Component {
             </div>
             <div>
               <button>Sign Up</button>
+              <div>
+                { authError ? <p>{authError}</p> : null }
+              </div>
             </div>
           </form>
         </div>
@@ -62,9 +66,15 @@ class SignUp extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    authError: state.auth.authError
   }
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser))
+  }
+}
 
-export default connect(mapStateToProps)(SignUp)
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
