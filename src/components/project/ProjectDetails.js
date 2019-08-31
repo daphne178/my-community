db1541d23af824efd1948c3348ecd82549adbf3b
daphne178 <realta23dc@gmail.com>
2019-08-31 18:34:24 -0400

    adding delete

--- src/components/project/ProjectDetails.js
+++ src/components/project/ProjectDetails.js
@@ -12,6 +12,27 @@ const ProjectDetails = (props) => {
   //   return <Redirect to='/signin' />
   // }
 
+  if(project && auth.uid === project.authorId){
+    return (
+      <div className="box-detail">
+        <div className="box detail-box">
+          <div>
+            <span className="title is-4">{project.title}</span>
+            <br/>
+            <br/>
+            <p className="is-1 has-text-black-ter">{project.content}</p>
+          </div>
+          <br/>
+          <div>
+            <div className="is-3 has-text-grey-dark">Posted by {project.authorFirstName} {project.authorLastName}</div>
+            <div className="is-6 has-text-grey">{moment(project.createdAt.toDate()).calendar()}</div>
+          </div>
+          <div>Delete</div>
+        </div>
+      </div>
+    )
+  }
+
   if(project){
     return (
       <div className="box-detail">
